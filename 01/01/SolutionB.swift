c671386e2816c57caf2c2a0414ed77bcdf5a7dd7
Russell Mirabelli <russell.mirabelli@bottlerocketstudios.com>
2023-12-01 08:44:36 -0600

    use tuples instead of dictionaries

--- 01/01/SolutionB.swift
+++ 01/01/SolutionB.swift
@@ -10,22 +10,22 @@ import AOCKit
 import Algorithms
 
 let table = [
-	["twone": "21"],
-	["threeight": "38"],
-	["eightwo": "82"],
-	["eighthree": "83"],
-	["nineight": "98"],
-	["sevenine": "79"],
-	["oneight": "18"],
-	["one" : "1"],
-	["two" : "2"],
-	["three" : "3"],
-	["four" : "4"],
-	["five" : "5"],
-	["six" : "6"],
-	["seven" : "7"],
-	["eight" : "8"],
-	["nine" : "9"],
+	("twone", "21"),
+	("threeight", "38"),
+	("eightwo", "82"),
+	("eighthree", "83"),
+	("nineight", "98"),
+	("sevenine", "79"),
+	("oneight", "18"),
+	("one", "1"),
+	("two", "2"),
+	("three", "3"),
+	("four", "4"),
+	("five", "5"),
+	("six", "6"),
+	("seven", "7"),
+	("eight", "8"),
+	("nine", "9"),
 ]
 
 // this is insanely gross
@@ -33,7 +33,7 @@ extension String {
 	func replaceWords() -> String {
 		var new = self
 		for element in table {
-			new = new.replacingOccurrences(of: element.first!.key, with: element.first!.value)
+			new = new.replacingOccurrences(of: element.0, with: element.1)
 		}
 		return new
 	}
